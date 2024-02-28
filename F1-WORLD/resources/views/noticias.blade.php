c165a6452b148ba84781aca725f2aabb740118cc
APEREZ2002 <119208468+APEREZ2002@users.noreply.github.com>
2024-02-28 15:20:03 +0100

    F1

---
+++ F1-WORLD/resources/views/noticias.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Noticias de Fórmula 1</title>
+    <link rel="stylesheet" href="../css/style.css">
+
+
+
+
+</head>
+<body>
+
+<header>
+    <div class="logo">
+    <img src="../images/1.jpeg" alt="Logo Fórmula 1">
+    </div>
+    <nav class="navbar">
+        <ul>
+            <li><a href="/">HOME</a></li>
+            <li><a href="/social">SOCIAL</a></li>
+            <li><a href="/store">STORE</a></li>
+            <li><a href="/experiencias">EXPERIENCIAS</a></li>
+            <li><a href="/carro">CARRO</a></li>
+            <li><a href="/login">LOGIN</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <section class="news">
+        <h1>Last News:</h1>
+        <div class="main-news">
+        <img src="../images/2.jpeg" alt="Noticia Principal">
+
+            <div class="news-content">
+                <h2>Título de la Noticia Principal</h2>
+                <p>Descripción breve de la noticia principal...</p>
+            </div>
+        </div>
+        <div class="secondary-news">
+            <a href="/news/1">
+                <img src="../images/3.jpeg" alt="Noticia 1">
+            </a>
+            <a href="/news/2">
+                <img src="../images/4.jpeg" alt="Noticia 2">
+            </a>
+            <a href="/news/3">
+                <img src="../images/3.jpeg" alt="Noticia 3">
+            </a>
+            <a href="/news/4">
+                <img src="../images/4.jpeg" alt="Noticia 4">
+            </a>
+        </div>
+    </section>
+</main>
+
+<footer>
+    <p>Tlf: 696 69 69 69</p>
+    <p>Mail: f69@f69.com</p>
+    <div class="footer-hours">
+        <p>Horario de atención al cliente:</p>
+        <p>09h-14h / 15h-18h</p>
+    </div>
+</footer>
+
+</body>
+</html>
