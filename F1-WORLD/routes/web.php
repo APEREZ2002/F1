6b0d60e7733ae9359f672a4ff6d983ceff094801
ABP18 <119179779+ABP18@users.noreply.github.com>
2024-03-07 18:54:59 +0100

    rutas actualizadas

--- F1-WORLD/routes/web.php
+++ F1-WORLD/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,42 @@ Route::get('/', function () {
     return view('noticias');
 });
 
+Route::get('/social', [ChatController::class, 'index'])->name('social.index');
+
 Route::get('/noticias/{id}', function ($id) {
     return view('noticias.noticia' . $id);
 });
+
+//Experiencias
+
+Route::get('/experiencias', function () {
+    return view('experiencias');
+});
+
+Route::get('/experiencias/{id}', function ($id) {
+    return view('experiencias.experiencias' . $id);
+});
+
+//social
+
+Route::get('/social', [ChatController::class, 'index'])->name('social.index');
+Route::post('/enviar-mensaje', [ChatController::class, 'enviarMensaje'])->name('enviar.mensaje');
+
+
+//store
+
+Route::get('/store', function () {
+    return view('store');
+});
+//perfil
+
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+
+
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
